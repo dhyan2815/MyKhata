20f650b7d0048683f3d386a8f881bbb7a5c8315b
dhyan2004 <liomad999@gmail.com>
2025-05-01 14:13:26 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,8 @@ const PORT = process.env.PORT || 5000;
 // For local backend server || 0.0.0.0 for Mobile backend server
 app.listen(PORT, '0.0.0.0', () => {
 
-  console.log(`Server running on port http://localhost:${PORT}`);
+  // console.log(`Server running on port http://localhost:${PORT}`);
 
   // For Render backend server
-  // console.log(`Server running on https://mykhata-backend.onrender.com`);
+  console.log(`Server running on https://mykhata-backend.onrender.com`);
 });
